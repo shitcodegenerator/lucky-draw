9cc33bdf86f2ee89ecf1805803a6f3d320659cde
shitcodegenerator <shitcodegenerator@gmail.com>
2022-08-19 11:04:43 +0800

    fix: remove unused file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from 'react'
-import './App.css'
-import styled from 'styled-components'
 import Home from './pages/Home'
-
-const AppWrapper = styled.div`
-  height: 100vh;
-  opacity: 0.7;
-  background-image: linear-gradient(#d7e5f7 0.9px, transparent 0.9px),
-    linear-gradient(to right, #d7e5f7 0.9px, #eef4ff 0.9px);
-  background-size: 18px 18px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+import { AppWrapper } from './App.style'
 
 function App() {
   return (
---
+++ src/App.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const AppWrapper = styled.div`
+  text-align: center;
+  height: 100vh;
+  opacity: 0.7;
+  background-image: linear-gradient(#d7e5f7 0.9px, transparent 0.9px),
+    linear-gradient(to right, #d7e5f7 0.9px, #eef4ff 0.9px);
+  background-size: 18px 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
